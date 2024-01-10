50e3361380c081bda63735fd2f2ba9cbfb97eece
GiacomoFranchi <giacomo.franchi.97@gmail.com>
2024-01-10 16:12:32 +0100

    add: info in cards

--- Views/partials/products.php
+++ Views/partials/products.php
@@ -3,23 +3,31 @@
         <?php foreach ($catalogo as $product){ ?>
         <div class="col"> 
             <div class="card">
-                
                 <div class="card-body">
+                    <span>Nome Articolo:</span>
                    <h2> <?php echo $product->get_name() ?> </h2>
-                   <p></p><?php echo $product->getAnimal() ?> <i class="<?php echo $product->getIcon()?>"> </i></p>
+                   <span>Categoria:</span>
                    <h4><?php echo $product->get_type() ?></h4>
+                   <p></p><?php echo $product->getAnimal() ?> <i class="<?php echo $product->getIcon()?>"> </i></p>
+                   <span>Prezzo:</span>
                    <h4><?php echo $product->get_price() ?>£</h4>
             
                 <?php if(is_a($product, 'Cuccia')){?>
+                    <span>MOdello cuccia</span>
                     <p><?php echo $product->get_modello() ?></p>
+                    <span>Dimensioni:</span>
                     <p><?php echo $product->get_dimensioni() ?></p>
+                    <span>Colore:</span>
                     <p><?php echo $product->get_colore() ?></p>
 
                     <?php } else if(is_a($product, 'Food')){?>
+                        <span>Data di scadenza:</span>
                         <p><?php echo $product->get_food_scad() ?></p>
                         
                     <?php } else if(is_a($product, 'Game')){?>
+                        <span>Forma:</span>
                         <p><?php echo $product->get_forma() ?></p>
+                        <span>Colore:</span>
                         <p><?php echo $product->get_colore() ?></p>
                     <?php } ?>
                 </div>
--- db.php
+++ db.php
@@ -20,21 +20,21 @@ $game2 = new Game ('gatto', 'fa-solid fa-cat'),
 
 $cuccia2 = new Cuccia ('gatto', 'fa-solid fa-cat'),
 ];
-$food1->set_poster('');
+$food1->set_poster('/img/img_negozio.jpg');
 $food1->setAnimal('cane');
 $food1->set_type('Cibo per cani');
 $food1->set_name('Purina-gnam');
 $food1->set_price('17.587');
 $food1->set_food_scad('2024-11-10');
 
-$game1->set_poster('');
+$game1->set_poster('/img/img_negozio.jpg');
 $game1->set_type('Giochi');
 $game1->set_name('Palla Pazza');
 $game1->set_price('7.50');
 $game1->set_forma('PpRay');
 $game1->set_colore('multicolor');
 
-$cuccia1->set_poster('');
+$cuccia1->set_poster('/img/img_negozio.jpg');
 $cuccia1->set_type('Cuccia');
 $cuccia1->set_name('CasaCane');
 $cuccia1->set_price('47.50');
@@ -42,20 +42,20 @@ $cuccia1->set_modello('Delux');
 $cuccia1->set_dimensioni('300x200x150');
 $cuccia1->set_colore('brown');
 
-$food2->set_poster('');
+$food2->set_poster('/img/img_negozio.jpg');
 $food2->set_type('Cibo per gatti');
 $food2->set_name('Purina-miaomiao');
 $food2->set_price('12.587');
 $food2->set_food_scad('2024-10-10');
 
-$game2->set_poster('');
+$game2->set_poster('/img/img_negozio.jpg');
 $game2->set_type('Giochi');
 $game2->set_name('topolino');
 $game2->set_price('3.50');
 $game2->set_forma('topo di fogna');
 $game2->set_colore('marrone');
 
-$cuccia2->set_poster('');
+$cuccia2->set_poster('/img/img_negozio.jpg');
 $cuccia2->set_type('Cuccia');
 $cuccia2->set_name('CasaGatto');
 $cuccia2->set_price('37.50');
